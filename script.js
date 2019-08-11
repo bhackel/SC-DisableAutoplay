f7bd3d73500c4f0a0f3e4954fe6ddc8e01bce204
Bryce Hackel <34104885+bhackel@users.noreply.github.com>
2019-08-11 13:36:52 -0700

    added functionality
    now continually disables the autoplay button after page load

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         soundcloud disable autoplay station
-// @version      1.0
-// @description  Opens the Soundcloud Next Up menu and turns off the Autoplay Station on load
+// @version      1.1
+// @description  Opens the Soundcloud Next Up menu and turns off the Autoplay Station on load, then continually disables it
 // @author       bhackel
 // @match        https://soundcloud.com/*
 // @grant        none
@@ -12,34 +12,45 @@
 (function() {
     'use strict';
 
-    function disableButton() {
-        // turns off the autoplay station button
-        var buttonDiv = document.getElementsByClassName("queueFallback__toggle").item(0);
-        if (buttonDiv) {
-            // check if autoplay station switch has loaded, then change its class to 'off' state
-            var button = buttonDiv.children.item(0);
-            button.className = "toggle sc-toggle";
-            toggleQueue(false);
-        } else {
-            // continue running until element is loaded
-            setTimeout(disableButton, 500);
-        }
-    }
-
-    function toggleQueue(first) {
-        // clicks to open/close the 'next up' menu
+    function openQueue() {
+        // opens the Next Up Queue to load the Autoplay button
         var queueTrigger = document.getElementsByClassName("playbackSoundBadge__queueCircle").item(0);
         if (queueTrigger) {
-            // check if 'next up' button has loaded, then click it
-            queueTrigger.click()
-            if (first) {
-                disableButton();
-            }
+            // check if next up button is loaded, then open it and call to close it
+            queueTrigger.click();
+            closeQueue(queueTrigger);
         } else {
-            // continue running until element is loaded
-            setTimeout(function(){ toggleQueue(first); }, 500);
+            // perform another check after delay
+            setTimeout(openQueue, 500);
         }
     }
 
-    toggleQueue(true);
+    function closeQueue(queueTrigger) {
+        // closes the Next Up queue once the Autoplay button has loaded
+        var buttonDiv = document.getElementsByClassName("queueFallback__toggle").item(0);
+        if (buttonDiv) {
+            // check if the button has loaded, then close the queue and call the loop
+            queueTrigger.click();
+            disableButtonLoop();
+        } else {
+            // perform another check after delay
+            setTimeout(function() { closeQueue(queueTrigger); }, 500);
+        }
+    }
+
+    function disableButtonLoop() {
+        // repeatedly tries to turn off the Autoplay button
+        var buttonDiv = document.getElementsByClassName("queueFallback__toggle").item(0);
+        if (buttonDiv) {
+            // make sure it exists to not error
+            var button = buttonDiv.children.item(0);
+            if (button.className === "toggle sc-toggle sc-toggle-active") {
+                // it has this classname when Autoplay is enabled
+                button.click();
+            }
+        }
+        setTimeout(disableButtonLoop, 2000);
+    }
+
+    openQueue();
 })();
